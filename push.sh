404593d19fc781f126c60b37d617df9c215b56ec
Dan <circa.1991@gmail.com>
2021-04-21 01:51:32 +0000

    updated

---
+++ push.sh
@@ -0,0 +1,21 @@
+trap ERR
+
+msg=$1
+
+if [[ $msg == "" ]]
+then 
+    echo "Please give commit message"
+    exit 1
+fi
+
+cargo build --release
+
+cp ./target/release/json-bucket .
+
+strip json-bucket
+
+git add *
+
+git commit -m "$msg"
+
+git push origin master
